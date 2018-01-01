c8f013264a7f536475b1112c8e604dc03dc5d473
Philipp Paulweber <p.paulweber@gmail.com>
2018-01-01 22:36:27 +0100

    Happy New Year :tada:

--- src/Function.cpp
+++ src/Function.cpp
@@ -1,9 +1,9 @@
 //
-//  Copyright (c) 2015-2017 Philipp Paulweber
+//  Copyright (C) 2015-2018 CASM Organization <https://casm-lang.org>
 //  All rights reserved.
 //
 //  Developed by: Philipp Paulweber
-//                https://github.com/casm-lang/libcjel-ir
+//                <https://github.com/casm-lang/libcjel-ir>
 //
 //  This file is part of libcjel-ir.
 //
@@ -19,6 +19,24 @@
 //
 //  You should have received a copy of the GNU General Public License
 //  along with libcjel-ir. If not, see <http://www.gnu.org/licenses/>.
+//
+//  Additional permission under GNU GPL version 3 section 7
+//
+//  libcjel-ir is distributed under the terms of the GNU General Public License
+//  with the following clarification and special exception: Linking libcjel-ir
+//  statically or dynamically with other modules is making a combined work
+//  based on libcjel-ir. Thus, the terms and conditions of the GNU General
+//  Public License cover the whole combination. As a special exception,
+//  the copyright holders of libcjel-ir give you permission to link libcjel-ir
+//  with independent modules to produce an executable, regardless of the
+//  license terms of these independent modules, and to copy and distribute
+//  the resulting executable under terms of your choice, provided that you
+//  also meet, for each linked independent module, the terms and conditions
+//  of the license of that module. An independent module is a module which
+//  is not derived from or based on libcjel-ir. If you modify libcjel-ir, you
+//  may extend this exception to your version of the library, but you are
+//  not obliged to do so. If you do not wish to do so, delete this exception
+//  statement from your version.
 //
 
 #include "Function.h"
